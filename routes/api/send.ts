eeb714d1cb46c74dd1938108249eb3119bcad4d2
Zizwar <zizwar@gmail.com>
2023-06-25 18:57:37 +0000

    async openai with whats

--- routes/api/send.ts
+++ routes/api/send.ts
@@ -2,39 +2,36 @@ import { Handlers } from "$fresh/server.ts";
 import { OpenAI } from "openai";
 
 export const handler: Handlers = {
-  async GET(_req, _ctx) {
-  //  const prompt = await req.text();
-    const openAI = new OpenAI(Deno.env.get("KEY_OPEN_AI") ?? "");
-/*
-    const completion = await openAI.createCompletion({
-      model: "davinci",
-      prompt: "what about in morocco",
-    }) ;
-    */
-    //console.log(completion.choices);
-    const SystemRoleContenet = "شات جي بي تي  انت تفهم جميع اللغاتوفي جميع المجالاتاي نص اعطيته لك افهمه وعالجه وورد على اي إستفسار لك رد باللهجة المغربية يمكنك كتابة كلمات تقنية لكن اكتبها بالدارجة المغربية، ركز الرد بالدارجة المغربية";
+  async POST(req, _ctx) {
+    try {
+      
+      const data = (await req.json());
+
+      const openAI = new OpenAI(Deno.env.get("KEY_OPEN_AI") ?? "");
     
-const userContent = "jock for animal dog";
-    const chatCompletion = await openAI.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { "role": "system", "content": SystemRoleContenet },
-        { "role": "user", "content": userContent},
+      const chatCompletion = await openAI.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { "role": "system", "content": SystemRoleContenet },
+          { "role": "user", "content": data.prompt},
         
-      ],
-    });
+        ],
+      });
     
-    console.log(chatCompletion);
+      console.log(chatCompletion);
 
-    const choices = chatCompletion?.choices;
+      const choices = chatCompletion?.choices;
 
-const text = choices[0]?.message.content;
+      const text = choices[0]?.message?.content ?? "gpt not respense";
 
-
-    return new Response(text, {
-      status: 200,
-    });
+      return new Response(text, {
+        status: 200,
+      });
+    } catch (error) {
+      console.error("Error occurred while processing request: ", error);
+      return new Response("Something went wrong!", { status: 500 });
+    }
   },
 };
 
-
+const SystemRoleContenet = "you are smart bot";
