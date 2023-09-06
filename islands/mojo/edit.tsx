a9b58ececf52387c03f95e030a2b783b913650d4
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-09-06 19:29:48 +0100

    Update edit.tsx

--- islands/mojo/edit.tsx
+++ islands/mojo/edit.tsx
@@ -14,7 +14,9 @@ export default function Edit({ data: { data = [] } }) {
     delete dataSubmit.token
     
     const UUID = dataSubmit.uuid;
-//if(UUID)delete dataSubmit.permissions;
+
+if(UUID) delete dataSubmit.permissions;
+
     console.log("start", { token,UUID, dataSubmit });
 
     const urlFetch = UUID
@@ -121,7 +123,7 @@ export default function Edit({ data: { data = [] } }) {
       type: "text",
       icon: "fa-hand-pointer",
     },  
-// { name: "permissions", label: "التصاريح", type: "textarea", icon: "fa-key" },
+{ name: "permissions", label: "التصاريح", type: "textarea", icon: "fa-key" },
     { name: "function", label: "الدالة", type: "textarea", icon: "fa-code" },
     { name: "rpc", label: "RPC", type: "text", icon: "fa-network-wired" },
     { name: "data", label: "البيانات", type: "textarea", icon: "fa-database" },
