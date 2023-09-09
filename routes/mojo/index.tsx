3ad8d6302994f305fd18f95ae5bfb5ba882371ac
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-09-09 21:03:41 +0100

    include lists mojo in index.tsx

--- routes/mojo/index.tsx
+++ routes/mojo/index.tsx
@@ -1,5 +1,29 @@
-import Lists from "@/islands/mojo/Llists.tsx";
+import { HandlerContext, PageProps } from "$fresh/server.ts";
 
-export default function Mojo() {
-  return <Lists/>;
+import {getTableMojoBy} from "@/mojo/witch.ts";
+
+import List from "@/islands/mojo/Llists.tsx";
+
+
+export default function Lists({ url, data, params }: PageProps) {
+  return <List data={data} />;
 }
+export async function handler(
+  req: Request,
+  ctx: HandlerContext
+): Promise<Response> {
+  
+  try {
+    response = await getTableMojoBy();
+
+
+    console.log({ response });
+
+  } catch (error) {
+    response = {};
+  }
+
+  return await ctx.render({ data: response });
+}
+
+
