6cf5046cb9c48e7627aeb40a2c5c3bbc4e03fc40
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-09-17 20:04:17 +0100

    Update edit.tsx

--- islands/mojo/edit.tsx
+++ islands/mojo/edit.tsx
@@ -61,8 +61,7 @@ dataSubmit.permissions = JSON.parse(dataSubmit.permissions);
     onChange={(e) => {
   
       const newValue = e.target.value === "true";
-      
-    }}
+   }})
    : type === "textarea" ? (
           <textarea
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
