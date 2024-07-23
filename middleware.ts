b4cb52085f0b0b0452b1839e6405b285bbd268b7
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-07-23 01:06:02 +0100

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { MiddlewareHandler } from "https://deno.land/x/hono@v4.0.9/mod.ts";
+
+export const corsMiddleware: MiddlewareHandler = async (ctx, next) => {
+  ctx.res.headers.set("Access-Control-Allow-Origin", "*");
+  ctx.res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  ctx.res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (ctx.req.method === "OPTIONS") {
+    ctx.res.status = 204;
+    return;
+  }
+
+  await next();
+};
